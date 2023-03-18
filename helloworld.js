81fcb7c523fc1f40e92ad90c09dfb2c45410648b
STADoedCodin <127996145+STADoedCodin@users.noreply.github.com>
2023-03-18 11:19:32 +0000

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,17 +1,9 @@
-(function(ext) {
-
-  // Define your blocks here
-  ext.say_hello = function() {
-    console.log("Hello, world!");
-  };
-
-  // Block and block menu descriptions
-  var descriptor = {
-    blocks: [
-      [' ', 'say hello', 'say_hello'],
-    ]
-  };
-
-  // Register the extension
-  ScratchExtensions.register('Hello World', descriptor, ext);
-})({});
+var descriptor = {
+  blocks: [
+    [' ', 'say hello', 'say_hello'],
+  ],
+  menus: {
+    myCategory: [' ', 'say hello', 'say_hello'],
+  },
+  url: 'https://example.com/extension.js'
+};
