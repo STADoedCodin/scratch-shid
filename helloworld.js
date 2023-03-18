0cb727bd852f1160397bde9de206a89f1a02610c
STADoedCodin <127996145+STADoedCodin@users.noreply.github.com>
2023-03-18 11:21:12 +0000

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,23 @@
-var descriptor = {
-  blocks: [
-    [' ', 'say hello', 'say_hello'],
-  ],
-  menus: {
-    myCategory: [' ', 'say hello', 'say_hello'],
-  },
-  url: 'https://example.com/extension.js'
-};
+(function(ext) {
+    // Extension code goes here
+    ext.sayHello = function() {
+        console.log("Hello");
+    };
+
+    // Block and descriptor for the extension
+    var descriptor = {
+        blocks: [
+            [' ', 'say hello', 'sayHello']
+        ],
+        menus: {
+            squidy: ['hello']
+        },
+        displayName: 'Squidy\'s Stuff',
+        color: '#B57EDC',
+        iconURL: 'https://i.imgur.com/2VHP9ty.png',
+        version: '1.0.0'
+    };
+
+    // Register the extension
+    ScratchExtensions.register('squidyStuff', descriptor, ext);
+})({});
